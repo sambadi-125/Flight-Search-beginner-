491964d1bc87cc6004d684fcc0484cb4bb9f37ca
Sambadi-125 <magomed.abubakarov@gmail.com>
2021-05-16 00:56:11 +0200

    Maga, posledniy update

--- src/com/magomed/tutorial/script.js
+++ src/com/magomed/tutorial/script.js
@@ -1,13 +1,13 @@
 var submit = document.getElementById("submit");
 
-submit.onclick = function() {
+function showInput() {
     var fromPlace = document.getElementById("from_place").value;
     var fromDate = document.getElementById("from_date").value;
     var toPlace = document.getElementById("to_place").value;
     var toDate = document.getElementById("to_place").value;
     var nrPassengers = document.getElementById("qty_passengers").value;
 
-    document.getElementById("depPl").innerHTML = "fromPlace";
+    document.getElementById("depPl").innerHTML = fromPlace;
     document.getElementById("departure_date").innerText = fromDate;
     document.getElementById("destination_place").innerText = toPlace;
     document.getElementById("destination_date").innerText = toDate;
